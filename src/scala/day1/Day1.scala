7974b17ec4b631a51e68d11bfdb426426faae2f5
Hannes Hertach <hanneshertach@gmail.com>
2019-12-14 15:43:08 +0000

    Implement day 1 part 2

--- src/scala/day1/Day1.scala
+++ src/scala/day1/Day1.scala
@@ -6,9 +6,15 @@ import scala.io.Source
 object Day1 {
 
   def main(args: Array[String]): Unit = {
+    println("Part 1:")
     println(fuelCounterUpper(loadInput()))
+    println("Part 2:")
+    println(sophisticatedFuelCounterUpper(loadInput()))
   }
 
+  /**
+    * Load input as list of integers
+    */
   val loadInput = () => {
     val numberList = ListBuffer[Int]()
     val source = Source.fromFile(getClass.getClassLoader.getResource("day1/input1").getPath)
@@ -21,5 +27,25 @@ object Day1 {
     numberList.toList
   }
 
+  /**
+    * Recursively calculate the fuel required by a single module
+    */
+  val recursiveFuelSingleModule: Int => Int = (module: Int) => {
+    val fuel = module / 3 - 2
+    fuel match {
+      case x if x < 0 => 0
+      case _ => fuel + recursiveFuelSingleModule(fuel)
+    }
+  }
+
+  /**
+    * Count the total fuel required for the simple calculation method
+    */
   val fuelCounterUpper = (modules: List[Int]) => modules.map(element => element / 3 - 2).sum
+
+  /**
+    * Count the total fuel required for the "sophisticated" method
+    */
+  val sophisticatedFuelCounterUpper = (modules: List[Int]) => modules.map(
+    module => recursiveFuelSingleModule(module)).sum
 }
